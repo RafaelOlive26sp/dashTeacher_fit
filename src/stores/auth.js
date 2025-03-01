2c0e8423981c4ca33431ceb470f1f5b15f3b0561
Rafael <oliveirarafael22@outlook.com.br>
2025-03-01 12:50:36 -0300

    add auth store with login functionality using Pinia

---
+++ src/stores/auth.js
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+import { login } from "@/services/auth";
+
+export const useAuthStore = defineStore('auth',{
+  state: () => ({
+    user: null,
+    token: null,
+  }),
+  getters: {
+    isAuthenticated: (state) => !!state.token,
+  },
+  actions:{
+    async login(response) {
+      try {
+        // console.log('response',response);
+        console.log(response);
+
+        this.token = response.access_token; // Acesse diretamente access_token
+        this.user = response.user; // Acesse diretamente user
+        localStorage.setItem('token', response.access_token); // Use access_token
+        localStorage.setItem('user', JSON.stringify(response.user));
+      } catch (error) {
+        console.error(error);
+        // throw new Error(error, 'Error logging in');
+
+      }
+    }
+  }
+})
