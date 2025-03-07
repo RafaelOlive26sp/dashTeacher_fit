c62f49259f08f32c66ff3c2b06dd8b3d90273295
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-03-07 20:35:27 -0300

    feat: metodo handleLogout

---
+++ src/pages/DashboardView.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-conteiner>
+    <v-row>
+      <v-col cols="12" sm="8" md="4">
+        <v-card>
+          <v-card-title>Dashboard</v-card-title>
+          <v-card-text>
+            <v-btn @click="handleLogout" color="primary" block>Logout</v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-conteiner>
+</template>
+
+
+<script setup>
+import {useAuthStore} from "@/stores/auth.js";
+import {useRouter} from "vue-router";
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+
+
+const handleLogout = async () => {
+  try{
+    await authStore.logout()
+    router.push('/login')
+  }catch (error) {
+    console.error('Error ao fazer o Logout ',error)
+  }
+}
+
+
+</script>
+
+
