3f8e7334cdb46c30d377662267abcb8c5a6088bf
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-03-07 20:37:04 -0300

    refactor: adicionado a declaracao de router e createdPinia

--- src/main.js
+++ src/main.js
@@ -13,8 +13,16 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 
+import { createPinia } from 'pinia'
+
+import router from "@/router/index.js";
+
+
+
 const app = createApp(App)
 
 registerPlugins(app)
+app.use(createPinia)
+app.use(router)
 
 app.mount('#app')
