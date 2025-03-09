856a41d7de0cb083894839fdabcbeb386c1f1490
Rafael <oliveirarafael22@outlook.com.br>
2025-03-08 21:06:41 -0300

    refactor: update DashboardView to enhance user greeting and remove logout functionality

--- src/pages/DashboardView.vue
+++ src/pages/DashboardView.vue
@@ -1,36 +1,28 @@
 <template>
-  <v-conteiner>
+  <v-conteiner class="border border-warning">
     <v-row>
-      <v-col cols="12" sm="8" md="4">
+      <v-col cols="12">
         <v-card>
           <v-card-title>Dashboard</v-card-title>
           <v-card-text>
-            <v-btn @click="handleLogout" color="primary" block>Logout</v-btn>
+            <p>Olá, {{authStore.user?.name}}</p>
+            <p>Seja bem vindo ao Dashboard</p>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
   </v-conteiner>
+
 </template>
 
 
 <script setup>
 import {useAuthStore} from "@/stores/auth.js";
-import {useRouter} from "vue-router";
-
 const authStore = useAuthStore()
-const router = useRouter()
 
 
 
-const handleLogout = async () => {
-  try{
-    await authStore.logout()
-    router.push('/login')
-  }catch (error) {
-    console.error('Error ao fazer o Logout ',error)
-  }
-}
+
 
 
 </script>
