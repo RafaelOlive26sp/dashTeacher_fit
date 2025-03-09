b847c1567fb6cb01c22fab5e36090e15c0a4e61a
Rafael <oliveirarafael22@outlook.com.br>
2025-03-09 02:55:25 -0300

    feat: add AppHeader component to display user greeting in the navigation bar

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+ <v-app-bar app height="40" elevation="2">
+    <small class="ml-2">Olá, {{ AuthStore.user?.name }}</small>
+  </v-app-bar>
+</template>
+
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+// import { NavBarLeft } from '@/components/NavBarLeft.vue';
+const AuthStore = useAuthStore();
+</script>
