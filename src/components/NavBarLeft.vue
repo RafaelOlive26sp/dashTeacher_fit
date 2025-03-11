fe2c0d0d21b327d03455b6c4da4fa7bb65f512c8
Rafael <oliveirarafael22@outlook.com.br>
2025-03-11 03:07:07 -0300

    feat: update NavBarLeft to include Pagamentos link for payments navigation

--- src/components/NavBarLeft.vue
+++ src/components/NavBarLeft.vue
@@ -10,7 +10,7 @@
       <v-divider></v-divider>
 
       <v-list density="compact" nav>
-        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+        <v-list-item prepend-icon="mdi-cash-clock" title="Pagamentos" to="/pagamentos" ></v-list-item>
         <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
         <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
       </v-list>
@@ -28,9 +28,9 @@
   import { useAuthStore } from '@/stores/auth';
   import { computed } from 'vue';
 
-
-
   const AuthStore = useAuthStore();
   const user = computed(() => AuthStore.user);
+
+
 
 </script>
