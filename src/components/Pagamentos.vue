b4654d7b5430c0f4b413eaad6fe1ddc3c0d9aa7d
Rafael <oliveirarafael22@outlook.com.br>
2025-03-11 03:06:58 -0300

    feat: add Pagamentos component to display pending and current payments

---
+++ src/components/Pagamentos.vue
@@ -0,0 +1,32 @@
+<template >
+  <v-conteiner>
+    <v-row>
+      <v-col cols="3">
+        <v-card>
+          <v-card-title>
+            <v-icon >mdi-cash-clock</v-icon>
+            <span>Pagamentos pendentes</span>
+          </v-card-title>
+          <v-card-text>
+            <v-list>
+
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="3">
+        <v-card>
+          <v-card-title>
+            <v-icon>mdi-cash-clock</v-icon>
+            <span>Pagamentos Vigentes</span>
+          </v-card-title>
+          <v-card-text>
+            <v-list>
+
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-conteiner>
+</template>
