d3ec5af4c4f487c553bf31d3fa3ca76a02e8404d
Rafael <oliveirarafael22@outlook.com.br>
2025-03-12 20:57:58 -0300

    feat: add expansions_Panel component for enhanced layout and functionality

---
+++ src/components/expansionsPanels/expansions_Panel.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-title>
+        <v-icon :color="props.colorIcon">{{ props.icon }}</v-icon>
+        <slot name="contentTitle"></slot>
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <slot name="content"></slot>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+
+</template>
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  colorIcon: {
+    type: String,
+    default: 'warning'
+  },
+  icon: {
+    type: String,
+    default: 'mdi-cash-clock'
+  }
+})
+
+</script>
+<style lang="">
+
+</style>
