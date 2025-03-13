8c39a35ecf7f61b14ee1d41b4fd57a56dd3c11de
Rafael <oliveirarafael22@outlook.com.br>
2025-03-13 03:44:50 -0300

    feat: enhance getPayments function to include authentication and improved error handling

--- src/services/user.js
+++ src/services/user.js
@@ -1,10 +1,21 @@
-import api from "js-cookie";
+import api from './api';
+import Cookies from 'js-cookie';
 
 export const getPayments = async () =>{
   try{
-    const response = await api.get('/api/v1/payments')
-    return response.data
+
+    const token = Cookies.get('access_token');
+    if(!token){
+      throw new Error('User not authenticated', 'Error getting payments');
+    }
+    const response = await api.get('/api/v1/payment/',{
+      headers:{
+        Authorization: `Bearer ${token}`
+      }
+    })
+    return response.data.data
   }catch(error){
     console.error(error);
+    throw new Error(error.response.data.message, 'Error getting payments');
   }
 }
