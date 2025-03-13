e75455c1eed50de4b0248ba59e050988711548d9
Rafael <oliveirarafael22@outlook.com.br>
2025-03-12 21:22:42 -0300

    feat: add tableView component for structured data display

---
+++ src/components/tables/tableView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-table height="300px" fixed-header>
+          <thead>
+            <tr>
+              <slot name="contentTHead" :class="props.classTh"></slot>
+            </tr>
+          </thead>
+          <tbody>
+            <slot name="contentTr"></slot>
+          </tbody>
+        </v-table>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+  classTh:{
+    type: String,
+    default: 'text-left'
+  }
+})
+</script>
