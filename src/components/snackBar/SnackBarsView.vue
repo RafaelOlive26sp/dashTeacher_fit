1ce0e4e5fd8094eeb064b68202e9168dc3da35ef
Rafael <oliveirarafael22@outlook.com.br>
2025-03-21 04:27:00 -0300

    feat: add SnackbarView component for displaying notifications

---
+++ src/components/snackBar/SnackBarsView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="text-center ma-2">
+    <!-- <v-btn @click="snackbar = true">
+      Open Snackbar
+    </v-btn> -->
+    <v-snackbar v-model="snackbar">
+      {{ props.textContent }}
+
+
+      <template v-slot:actions>
+        <v-btn color="pink" variant="text" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+
+</template>
+
+
+<script setup>
+  import { ref } from 'vue'
+
+  const props = defineProps({
+    textContent:{
+      type: String,
+      default: 'Hello, I am a snackbar'
+    }
+  })
+
+  const snackbar = ref(false)
+  // const text = ref(`Hello, I'm a snackbar`)
+
+</script>
