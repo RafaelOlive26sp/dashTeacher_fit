c4d9a3be2c4d2e9b0a2bab35a1d7a3ec336f02a0
Rafael <oliveirarafael22@outlook.com.br>
2025-03-21 04:26:54 -0300

    feat: update EditAccount component with new fields for user details and improved data handling

--- src/components/EditAccount.vue
+++ src/components/EditAccount.vue
@@ -1,18 +1,21 @@
 <template>
   <v-row dense>
     <v-col cols="12" md="4" sm="6">
-      <v-text-field label="First name*" required></v-text-field>
+      <v-text-field label="Nome" v-model="editAccount.name"></v-text-field>
     </v-col>
 
     <v-col cols="12" md="4" sm="6">
-      <v-text-field hint="example of helper text only on focus" label="Middle name"></v-text-field>
+      <v-text-field hint="1.75" label="Altura" v-model="editAccount.height" type="number"></v-text-field>
     </v-col>
 
     <v-col cols="12" md="4" sm="6">
-      <v-text-field hint="example of persistent helper text" label="Last name*" persistent-hint required></v-text-field>
+      <v-text-field hint="65.00" label="Peso*"  required v-model="editAccount.weight" type="number"></v-text-field>
+    </v-col>
+    <v-col cols="12" md="4" sm="6">
+      <v-text-field label="Condição Medica*" persistent-hint required v-model="editAccount.medical_condition"></v-text-field>
     </v-col>
 
-    <v-col cols="12" md="4" sm="6">
+    <!-- <v-col cols="12" md="4" sm="6">
       <v-text-field label="Email*" required></v-text-field>
     </v-col>
 
@@ -22,20 +25,20 @@
 
     <v-col cols="12" md="4" sm="6">
       <v-text-field label="Confirm Password*" type="password" required></v-text-field>
-    </v-col>
+    </v-col> -->
 
-    <v-col cols="12" sm="6">
+    <!-- <v-col cols="12" sm="6">
       <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
-    </v-col>
+    </v-col> -->
 
-    <v-col cols="12" sm="6">
+    <!-- <v-col cols="12" sm="6">
       <v-autocomplete
         :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
         label="Interests" auto-select-first multiple></v-autocomplete>
-    </v-col>
+    </v-col> -->
   </v-row>
 
-  <small class="text-caption text-medium-emphasis">*indicates required field</small>
+  <small class="text-caption text-medium-emphasis">*Campos Obrigatorios</small>
 
 
   <v-divider></v-divider>
@@ -46,13 +49,20 @@
 
     <v-btn color="primary" text="Save" variant="tonal" @click="saveDialog"></v-btn>
   </v-card-actions>
-  {{ dialogVisible }}
+
 
 </template>
 
 
 <script setup>
 import { ref } from 'vue';
+
+const editAccount = ref({
+  name: "",
+  height: "",
+  weight: "",
+  medical_condition: "",
+});
 
 const props = defineProps({
   dialogVisible: Boolean,
@@ -68,7 +78,15 @@ const closeDialog = () => {
 }
 
 const saveDialog = () => {
-  console.log('Save dialog');
+  // console.log('Save dialog', editAccount.value);
+  let data = {
+    name: editAccount.value.name,
+    height: editAccount.value.height,
+    weight: editAccount.value.weight,
+    medical_condition: editAccount.value.medical_condition,
+  }
+  console.log('Data', data);
+
   emit('update:dialogVisible', false);
 }
 
