537ba69ad449212583503a0f32f580c092221c23
Rafael <oliveirarafael22@outlook.com.br>
2025-04-04 03:37:06 -0300

    feat: add CreatedScheduleView component with class loading functionality

---
+++ src/components/CreatedScheduleView.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>estamos em created Schedule view</h2>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { loadClasses as loadClassesApi } from '@/services/user';
+const classSchedules = ref([])
+
+onMounted(async () => {
+  await loadDataClasses();
+})
+
+
+const loadDataClasses = async () => {
+  try {
+    const response = await loadClassesApi()
+    classSchedules.value = response.data
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+
+
+
+
+
+</script>
+
