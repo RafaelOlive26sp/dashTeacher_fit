9339ccffa9ea0ef0f75dcd54baf28fabd3edd4dd
Rafael <oliveirarafael22@outlook.com.br>
2025-04-08 03:21:00 -0300

    feat: enhance SchedulesView with loading state and improved snackbar actions

--- src/components/SchedulesView.vue
+++ src/components/SchedulesView.vue
@@ -1,37 +1,43 @@
-
-
 <template>
   <v-form @submit.prevent="submitForm" ref="form">
-    <v-select
-      v-model="newClass.day_of_week"
-      :items="weekDays"
-      label="Dia da Semana"
-      item-title="label"
-      item-value="value"
-      required
-    ></v-select>
+    <v-select v-model="newClass.day_of_week" :items="weekDays" label="Dia da Semana" item-title="label"
+      item-value="value" required></v-select>
 
-    <v-text-field
-      v-model="newClass.start_time"
-      label="Horário de Início"
-      type="time"
-      required
-    ></v-text-field>
-    <v-btn type="submit" color="primary">Criar</v-btn>
+    <v-text-field v-model="newClass.start_time" label="Horário de Início" type="time" required></v-text-field>
+    <div class="text-center">
+      <v-btn type="submit" :loading="loading" >
+        Criar Aula
+
+        <template v-slot:loader>
+          <v-progress-linear indeterminate></v-progress-linear>
+        </template>
+      </v-btn>
+    </div>
   </v-form>
+  <SnackBarsView :textContent="snackbarMessage" v-model="snackbar">
+    <template #actions>
+      <v-btn color="pink" variant="text" @click="snackbar = false">
+        Close
+      </v-btn>
+    </template>
+  </SnackBarsView>
 </template>
 
 
 <script setup>
 import { ref, defineProps, watch, onMounted } from 'vue';
-import {useUserStore} from "@/stores/user.js";
-import {createdScheduleInClass as createdScheduleInClassApi} from "@/services/user.js";
+// import { useUserStore } from "@/stores/user.js";
+import { createdScheduleInClass as createdScheduleInClassApi } from "@/services/user.js";
 
 
-const userStore = useUserStore();
+// const userStore = useUserStore();°
+const snackbarMessage = ref('');
+const snackbar = ref(false)
 const props = defineProps({
   classId: Number
 });
+const emit = defineEmits(['close', 'refresh-data']);
+const loading = ref(false)
 
 const newClass = ref({
   day_of_week: '',
@@ -67,13 +73,26 @@ watch(
   },
   { immediate: true }
 );
+// watch(loading, val =>{
+//   if(val) return
+//     setTimeout(() => {
+//       loading.value = false
+//     }, 2000);
+// })
 
 const submitForm = async () => {
   try {
     // console.log('NewClass enviando para services ',newClass.value);
-      const response = await createdScheduleInClassApi(newClass.value);
+    const response = await createdScheduleInClassApi(newClass.value);
     console.log('resposta da api dentro da view ', response);
-  }catch (e) {
+    snackbarMessage.value = response.message
+    snackbar.value = true
+    loading.value = true
+    emit('refresh-data');
+    setTimeout(() => {
+      emit('close');
+    }, 2000)
+  } catch (e) {
     console.log(e)
   }
 };
