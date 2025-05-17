b136810339827336a9e193eb6ae029fd2f3f7403
Rafael Delgado <oliveirarafael22@outlook.com.br>
2025-05-17 10:21:05 -0300

    refactor: Limpeza de logs

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,11 +2,7 @@ import api from "./api";
 
 
 export const login = async (credentials) => {
-
-
   const response = await api.post('/api/login', credentials)
-  console.log('response da api',response.data);
-
   return response.data
 }
 
--- src/stores/user.js
+++ src/stores/user.js
@@ -26,97 +26,61 @@ export const useUserStore = defineStore('user',{
   },
   actions:{
     async getPayment(response){
-      try{
-        // console.log('estamos em getPayments em actions', response);
-        this.payments = response;
-
-
-      }catch(error){
-        console.error(error);
-      }
+      this.payments = response;
     },
     async getStudentsWithUser(response){
-      try{
-        // console.log('estamos em getStudentsWithUser em actions', response);
-        this.students = response;
-      }catch(error){
-        console.error(error);
-        // throw new Error("error ao pegar estudantes", 'Error getting students', error);
 
-      }
+
+        this.students = response;
+
     },
     async removeUser(response) {
-      try {
-        // console.log('response dentro de store ', response);
 
         this.responseRemoveUser = response;
-      } catch (error) {
-        console.log(error);
 
-      }
     },
     async usersWithIncompleteProfile(response){
-      try{
+
 
         this.usersIncompleteProfile = response.data;
 
-      }catch(error){
-        console.log(error);
-      }
+
     },
     async fetchPayments(response){
-      try{
-      // console.log('estamos dentro de fetchPayments ',response)
+
         this.schedulesPaymentUsers = response.data
 
-      }catch(error){
-        console.error('error em store',error);
-      }
+
     },
     async confirmPaymentStore(response){
-      try {
-        this.paymentConfirm = response.data
-      } catch (error) {
-        console.log(error);
 
-      }
+        this.paymentConfirm = response.data
+
     },
     async loadDataSchedules(response){
-      try {
+
         this.loadClasses = response.data
-        // console.log('dados de loadDataSchedules ', response.data);
 
-      } catch (error) {
-        console.log(error);
 
-      }
     },
     async loadCreatedClass(response){
-      try {
-        // console.log('resposta do back dentro de store ', response);
-        this.loadClassesCreate = response
-      } catch (error) {
-        console.log(error);
 
-      }
+        this.loadClassesCreate = response
+
     },
     async successAppointmentUser(response){
-      try {
+
         this.successAppointmentUserData = response
-      }catch (e) {
-        console.log(e)
-      }
+
     },
     async getDataScheduleStore(response){
-      try{
+
         this.dataScheduleStoreUsersClass = response
-      }catch (e) {
-        console.log(e)
-      }
+
     },
     triggerNavBarRefresh(){
       this.refreshNavBar++
-      console.log('estamos no trigger');
+
 
     }
 
